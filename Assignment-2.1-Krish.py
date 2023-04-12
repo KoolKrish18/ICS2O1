35fec092fd44c1dc74f29a207b48f106c2ca9c72
Krish Nalam <107896377+KoolKrish18@users.noreply.github.com>
2023-04-12 16:40:06 -0400

    Add files via upload

---
+++ Assignment-2.1-Krish.py
@@ -0,0 +1,45 @@
+'''
+Name: Krish Nalam
+Date: Febuary 17th 2021
+Program Title:  Assignment2.py
+Program Description:  My program intends to follow the instructions given in D2L for assignment 2 and perform the given tasks  
+Last Modified: Feburary 22th 2021
+'''
+
+x = 123
+#this is storing the number 123 in the variable x
+print(x)
+#this code prints the variable x on to the screen
+Hello = "Hello"
+World = "World!"
+#this is storing string values Hello and World! in 2 different variables 
+print("\n")
+print(Hello + "  " + World)
+#this code prints the variables Hello and World on to the screen which read the string values Hello and World!
+y = "42"
+#this is storing the string value 42 in the variable y
+print("\n")
+print("What is the answer to the question of life, the universe and everything?",y,"!")
+#this code prints the question What is the answer to the question of life, the universe and everything? along with the answer stored in the variable y
+print("\n")
+Age = input("Enter your age: ")
+#this code asks the user to enter their age, inputing it into the variable Age
+print("You are " + Age + " years old.")
+#this code prints a sentence repeating what the entered age was stating that You are, the age entered, years old
+print("\n")
+print("Please enter your birthday below")
+Month = input("Enter your birth month: ")
+Date = input("Enter your birth day: ")
+Year = input("Enter your birth year: ")
+#this code prompts the user to enter their date and storesthe month, date and year in different vairables
+print("Your birthday is " + Month + " " + Date + " " + Year)
+#this code reiterates the birthday of the user on the console
+print("\n")
+Symbol = input("Enter any single symbol:")
+#this code asks the user to enter a symbol
+print(Symbol + Symbol + Symbol + Symbol + Symbol)
+#This code repeats the enetered symbol 5 times
+SymbolTwo = input("Enter another single symbol:")
+print("\n")
+print ( SymbolTwo + "          " + SymbolTwo + "       " + SymbolTwo + "          " + SymbolTwo, "\n", SymbolTwo + "         " + SymbolTwo + "        " + SymbolTwo + SymbolTwo + "         " +  SymbolTwo, "\n", SymbolTwo + "        " + SymbolTwo + "         " + SymbolTwo + "  " + SymbolTwo + "       " +  SymbolTwo, "\n", SymbolTwo + "       " + SymbolTwo + "          " + SymbolTwo + "   " + SymbolTwo + "      " + SymbolTwo, "\n", SymbolTwo + "      " + SymbolTwo + "           " + SymbolTwo + "     " + SymbolTwo + "    " + SymbolTwo, "\n", SymbolTwo + "     " + SymbolTwo + "            " + SymbolTwo + "       " + SymbolTwo + "  " + SymbolTwo, "\n", SymbolTwo + "    " + SymbolTwo + "             " + SymbolTwo + "         " + SymbolTwo + SymbolTwo, "\n", SymbolTwo + "     " + SymbolTwo + "            " + SymbolTwo + "          " + SymbolTwo, "\n", SymbolTwo + "      " + SymbolTwo + "           " + SymbolTwo + "          " + SymbolTwo, "\n", SymbolTwo + "       " + SymbolTwo + "          " + SymbolTwo + "          " + SymbolTwo, "\n", SymbolTwo + "        " + SymbolTwo + "         " + SymbolTwo + "          " + SymbolTwo)
+#this code uses another symbol as the input to output a display of my intials with said symbol 
