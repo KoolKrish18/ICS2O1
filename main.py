408f9df0b4ab8294bbf94cbf2b7b9a6a3819aac6
Krish Nalam <107896377+KoolKrish18@users.noreply.github.com>
2023-04-12 08:26:44 -0400

    Assignment 1 Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+''' 
+Name: Krish Nalam
+Date: Febuary 16th 2021
+Program Title:  Assignment2-1.py
+Program Description:  My program intends to follow the instructions given in D2L and perform the given tasks  
+Last Modified: Feburary 17th 2021
+'''
+
+print("Hello World!");
+#This line is printing Hello World!
+print();
+print("Krish Nalam");
+#This line is printing Krish Nalam!
+print();
+print("This programming stuff is \"easy\"!");
+#This line is printing This programming stuff is “easy”!
+print();
+print("The file is found at \"c:\program files\python\"");
+#This line is printing The file is found at “c:\program files\python”
+print();
+print("Donald A Wilson \nSecondary \nSchool");
+#This line is printing Donald A Wilson Secondary School 
+print();
+print();
+print();
+print("*      *    *          *\n*    *      *  *       * \n* *         *    *     * \n*    *      *       *  * \n*      *    *          *");
+print();
+print();
+print();
+print("* * * * * * \t      * \t            *\n*         * \t    *   * \t          *      * \n*         * \t   *     * \t         *\n*         * \t  *       * \t      *      *\n* * * * * * \t * * * * * * \t        *")
